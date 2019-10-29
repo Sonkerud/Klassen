7e22ded438644996081df2272a6aca7281acd16d
alexander sonerud <alexandersonerud@hotmail.com>
2019-10-29 13:29:10 +0100

    ff

--- Klassen/Program.cs
+++ Klassen/Program.cs
@@ -5,18 +5,21 @@ using System.Linq;
 namespace Klassen
 {
     class Program
-    {   static List<string> klassen = new List<string>();
+    {
+        static List<string> klassen = new List<string>();
+        static List<int> intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         static Random random = new Random();
-    
+        static Dictionary<int, List<string>> gruppering = new Dictionary<int, List<string>>();
         static void Main(string[] args)
         {
             AddData();
             PresentData();
             Attendees();
-            PresentRandomGroups(klassen.ChunkBy(klassen.Count / NumberOfGroups(klassen)));
+            PresentRandomGroups(GroupClass(klassen, NumberOfGroups(klassen)));
             PresentGroupToDoPresentation(ChoseGroupForPresentation(NumberOfGroups(Randomize(klassen))));
-        }
 
+
+        }
         static void AddData()
         {
             klassen.Add("Alexander Sonerud");
@@ -37,45 +40,42 @@ namespace Klassen
             Console.WriteLine("Klass 9A - 2003\n");
             for (int i = 0; i < klassen.Count; i++)
             {
-                Console.WriteLine($"{i+1}. {klassen[i]}");
+                Console.WriteLine($"{i + 1}. {klassen[i]}");
             }
         }
         static void Attendees()
-       {
+        {
             Console.WriteLine("\nAnge antal ej närvarande elever: ");
             int numberOfNotAttendingPupils = int.Parse(Console.ReadLine());
             for (int i = 0; i < numberOfNotAttendingPupils; i++)
             {
                 Console.WriteLine("\nAnge nr på ej närvarande elev:");
                 int pupilNotHere = int.Parse(Console.ReadLine());
-                klassen.RemoveAt(pupilNotHere -1);
+                klassen.RemoveAt(pupilNotHere - 1);
                 Console.Clear();
                 PresentData();
             }
-       }
-        static void PresentRandomGroups(List<List<string>> sortedKlass)
+        }
+        static void PresentRandomGroups(Dictionary<int, List<string>> sortedKlass)
         {
             int x = 1;
             Console.WriteLine("\nDagens gruppindelning:\n");
             foreach (var item in sortedKlass)
             {
-                Console.WriteLine($"Grupp {x}");
-                foreach (var person in item)
+                Console.WriteLine($"\nGrupp: {x}");
+                foreach (var person in item.Value)
                 {
                     Console.WriteLine(person);
                 }
-                Console.WriteLine();
                 x++;
             }
         }
-      
         static List<string> Randomize(List<string> list)
         {
             var models = list.OrderBy(c => random.Next()).Select(c => c).ToList();
-                      
+
             return models;
         }
-        
         static int NumberOfGroups(List<string> list)
         {
             int nr = list.Count;
@@ -83,74 +83,85 @@ namespace Klassen
             if (nr % 3 == 0)
             {
                 delat = nr / 3;
-            
+
                 return delat;
             }
             else if (nr % 3 == 2)
             {
                 delat = nr / 3;
-         
+
                 return delat;
 
             }
             else if (nr % 3 == 1)
             {
-                delat = nr / 2; 
-            
+                delat = nr / 2;
+
                 return delat;
             }
             return 0;
         }
         static int ChoseGroupForPresentation(int nrOfGroups)
         {
-            return random.Next(1, nrOfGroups+1);
+            return random.Next(1, nrOfGroups + 1);
         }
         static void PresentGroupToDoPresentation(int groupToDoPresentation)
         {
-            Console.WriteLine("Vilka får äran att presentera idag? - Tryck Enter");
+            Console.WriteLine("\nVilka får äran att presentera idag? - Tryck Enter");
             Console.ReadLine();
             Console.WriteLine($"Grupp nr {groupToDoPresentation}! Tjoho!");
             Console.ReadLine();
         }
 
-        ////static void ShowGroups(List<string> klassen)
-        //{
-        //    if (klassen.Count % 3 == 0 || klassen.Count % 3 == 2)
-        //    {
-        //        for (int i = 0; i < klassen.Count; i++)
-        //        {
-        //            if (i % 3 == 0)
-        //            {
-        //                Console.WriteLine($"\nGrupp {x}");
-        //                x++;
-        //            }
-        //            Console.WriteLine(klassen[i]);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        for (int i = 0; i < klassen.Count; i++)
-        //        {
-        //            int x = 1;
-        //            if (i % 2 == 0)
-        //            {
-        //                Console.WriteLine($"\nGrupp{x}");
-        //                x++;
-        //            }
-        //            Console.WriteLine(klassen[i]);
-        //        }
-        //    }
-        //}
+        static Dictionary<int, List<string>> GroupClass(List<string> list, int chunk)
+        {
+            //Randomiserad lista 
+            var newList = Randomize(list);
+
+            for (int i = 0; i <= list.Count / chunk; i++)
+            {
+                if (newList.Count >= chunk)
+                {
+                    gruppering.Add(i, newList.Take(chunk).ToList());
+
+                    for (int x = 0; x < chunk; x++)
+                    {
+                        newList.RemoveAt(0);
+                    }
+                }
+                else if (newList.Count != 0)
+                {
+                    gruppering.Add(i, newList.Take(newList.Count).ToList());
+                }
+            }
+            return gruppering;
+        }
     }
+
+    //Select 1: Lägg till index till varje value. x = string, i = int(indexvärde). 
+    //Anonym funktion sätter värden till variablarna Value och Index. 
+    //GroupBy: Gruppera listan i nya listor baserat på indexvärdet. 
+    //Select 2: 
     public static class ListExtensions
     {
         public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
         {
             return source
-                .Select((x, i) => new { Index = i, Value = x })
+                .Select((x, i) => new { Value = x, Index = i })
                 .GroupBy(x => x.Index / chunkSize)
                 .Select(x => x.Select(v => v.Value).ToList())
                 .ToList();
         }
+
+        public static List<List<T>> ChunkBy2<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Value = x, Index = i })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
     }
+
 }
